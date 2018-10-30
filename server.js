fe0dfb82198395181996d5d9a5504cd6eb92749b
kiran <kiran@neostack.com>
2018-10-30 22:28:54 +0530

    mutation works

---
+++ server.js
@@ -0,0 +1,42 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+function sleep(delay) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+const typeDefs = gql`
+  type User {
+    id: Int
+    name: String
+  }
+  type Mutation {
+    updateName(name: String): User
+  }
+  type Query {
+    user: User
+  }
+`;
+
+let user = {
+  id: 1,
+  name: "john"
+};
+
+const resolvers = {
+  Query: {
+    user: () => user
+  },
+  Mutation: {
+    updateName: async (root, args) => {
+      await sleep(10);
+      user.name = args.name;
+      return user;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+import { ApolloProvider, Query, Mutation, graphql } from "react-apollo";
+import { load } from "protobufjs";
+import { runInThisContext } from "vm";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const userQuery = gql`
+  {
+    user {
+      id
+      name
+    }
+  }
+`;
+
+const mutationQuery = gql`
+  mutation updateMutation($name: String) {
+    updateName(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+class Name extends React.Component {
+  changeHandler(e) {
+    client
+      .mutate({
+        mutation: mutationQuery,
+        variables: { name: e.target.value }
+      })
+      .then(res => {
+        console.log("mutaiton res", res);
+      });
+  }
+  render() {
+    const { user, loading } = this.props.data;
+    if (loading) {
+      return null;
+    }
+    return (
+      <div>
+        {user.name}
+        <input
+          placeholder="name"
+          value={user.name}
+          onChange={this.changeHandler}
+        />
+      </div>
+    );
+  }
+}
+
+const NameContainer = graphql(userQuery)(Name);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div>Hello</div>
+          <NameContainer />
+          {/* <NameInput /> */}
+        </div>
+      </ApolloProvider>
     );
   }
 }
